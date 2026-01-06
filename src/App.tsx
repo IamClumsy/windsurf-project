f443aa9c0f0a872c3e0ae2bb029ab986f47cf877
Mike Brown <mick@Micklar.local>
2026-01-06 17:29:35 -0500

    Legend: make 'Gold Gathering Skill' text white

--- src/App.tsx
+++ src/App.tsx
@@ -618,7 +618,7 @@ function App() {
                 <span className="inline-flex items-center px-3 py-1.5 rounded-full text-xs font-bold bg-gradient-to-r from-slate-600 to-slate-700 gold-text">
                   Orange&#9;
                 </span>
-                <span className="text-white text-sm" style={{color: '#fb923c', fontWeight: 700}}>Gold Gathering Skill</span>
+                <span className="text-white text-sm font-bold">Gold Gathering Skill</span>
               </div>
           </div>
         </div>
